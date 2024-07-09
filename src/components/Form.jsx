97ba26cb3298ca06705f080cfa4eb66e1c6d38af
Neil T. Rigaud <ntsrigaud14@gmail.com>
2024-07-09 17:38:30 +0800

    Removed unused modules

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useState } from "react";
 import styles from "../styles/Form.module.css";
 import { AppContext } from "../App";
 
